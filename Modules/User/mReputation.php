f0811b506d5689c6893ab35babda7d3d70cd58aa
Dzmitry Kosmach <dzmitry.kosmach@gmail.com>
2025-08-25 18:36:02 +0300

    local24

--- Modules/User/mReputation.php
+++ Modules/User/mReputation.php
@@ -1,23 +1,27 @@
 <?php
-
 class ModuleMain
 {
   public static function main($pageInf)
   {
-    // Получаем путь к шаблону
+    // Отладка
+    error_log('mReputation module called');
+    error_log('pageInf: ' . print_r($pageInf, true));
+
     $tpl = Config::path('skins') . '/html/User/mReputation.htm';
 
-    // Добавим отладку
+    // Проверка существования шаблона
     if (!file_exists($tpl)) {
+      error_log('Template not found: ' . $tpl);
       die('Template not found: ' . $tpl);
     }
 
-    // Вставляем переменные
-    return pattExeP(fgc($tpl), array(
-      'pageInf' => $pageInf,
-      'title' => $pageInf['name'] ?? 'Репутация'
-    ));
+    // Отладка данных
+    $data = array(
+        'pageInf' => $pageInf,
+        'title' => $pageInf['name'] ?? 'Репутация'
+    );
+    error_log('Template data: ' . print_r($data, true));
+
+    return pattExeP(fgc($tpl), $data);
   }
 }
-
-?>
