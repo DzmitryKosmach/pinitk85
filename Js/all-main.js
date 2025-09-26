9d73057afd61b672e874546e058f7007f9b337ed
Dzmitry Kosmach <dzmitry.kosmach@gmail.com>
2025-09-26 15:04:45 +0300

    local147

--- Js/all-main.js
+++ Js/all-main.js
@@ -2156,67 +2156,98 @@ var oMaterials = new function(){
 /**
  * Сравнение серий
  */
-var oCompare = new function(){
-	/**
-	 * {string}
-	 */
-	this.url = '';
+/**
+ * Сравнение серий
+ */
+var oCompare = new function() {
+  /**
+   * {string}
+   */
+  this.url = '';
 
 
-	/**
-	 * @param	{int}	seriesId
-	 */
-	this.add = function(seriesId, rnd){
-		$$$('series-compare-' + seriesId + '-0').style.display = 'none';
-		$$$('series-compare-' + seriesId + '-1').style.display = '';
+  /**
+   * @param  {int}  seriesId
+   */
+  this.add = function (seriesId) {
+    // Находим все кнопки "Добавить" для этого товара
+    var addButtons = document.querySelectorAll('.series-compare-add[data-id="' + seriesId + '"]');
+    var removeButtons = document.querySelectorAll('.series-compare-remove[data-id="' + seriesId + '"]');
 
-		AJAX.lookup(
-			this.url + '?add=' + seriesId + '&ajax=1',
-			function(cnt){
-				$$$('series-compare-cnt').innerHTML = cnt;
-				var spans = find('span.series-compare-cnt');
-				for(var i = 0, l = spans.length; i < l; i++){
-					if(typeof(spans[i]) === 'undefined') continue;
-					spans[i].innerHTML = cnt;
-				};
-				// new code
-				$$$('favorinfo').innerHTML = cnt;
-				if (cnt > 0 && $('.compare-items').hasClass('noitems')) {
-					$('.compare-items').toggleClass('hide');
-					$('.compare-items').removeClass('noitems');
-				}
-				// new code
-			}
-		);
-	};
+    // Скрываем "Добавить", показываем "Убрать"
+    addButtons.forEach(function (btn) {
+      btn.style.display = 'none';
+    });
+    removeButtons.forEach(function (btn) {
+      btn.style.display = '';
+    });
 
+    // Отправляем AJAX
+    AJAX.lookup(
+      this.url + '?add=' + seriesId + '&ajax=1',
+      function (cnt) {
+        // Обновление счётчиков (оставьте как есть)
+        $$$('series-compare-cnt').innerHTML = cnt;
+        var spans = find('span.series-compare-cnt');
+        for (var i = 0, l = spans.length; i < l; i++) {
+          if (typeof spans[i] !== 'undefined') spans[i].innerHTML = cnt;
+        }
 
-	/**
-	 * @param	{int}	seriesId
-	 */
-	this.remove = function(seriesId){
-		$$$('series-compare-' + seriesId + '-0').style.display = '';
-		$$$('series-compare-' + seriesId + '-1').style.display = 'none';
+        // Обновление бейджей в header (оставьте как есть)
+        var desktopBadge = $$$('compare-badge-count-desktop');
+        var mobileBadge = $$$('compare-badge-count-mobile');
+        if (desktopBadge) {
+          desktopBadge.innerHTML = cnt;
+          cnt > 0 ? desktopBadge.classList.remove('hidden') : desktopBadge.classList.add('hidden');
+        }
+        if (mobileBadge) {
+          mobileBadge.innerHTML = cnt;
+          cnt > 0 ? mobileBadge.classList.remove('hidden') : mobileBadge.classList.add('hidden');
+        }
+      }
+    );
+  };
 
-		AJAX.lookup(
-			this.url + '?remove=' + seriesId + '&ajax=1',
-			function(cnt){
-				var spans = find('span.series-compare-cnt');
-				for(var i = 0, l = spans.length; i < l; i++){
-					if(typeof(spans[i]) === 'undefined') continue;
-					spans[i].innerHTML = cnt;
-				};
-				// new code
-				$$$('favorinfo').innerHTML = cnt;
-				if (cnt == 0) {
-					$('.compare-items').toggleClass('hide');
-					$('.compare-items').addClass('noitems');
-				};
-				// new code
-			}
-		);
-	};
-};
+  /**
+   * @param  {int}  seriesId
+   */
+  this.remove = function (seriesId) {
+    var addButtons = document.querySelectorAll('.series-compare-add[data-id="' + seriesId + '"]');
+    var removeButtons = document.querySelectorAll('.series-compare-remove[data-id="' + seriesId + '"]');
+
+    // Скрываем "Убрать", показываем "Добавить"
+    removeButtons.forEach(function (btn) {
+      btn.style.display = 'none';
+    });
+    addButtons.forEach(function (btn) {
+      btn.style.display = '';
+    });
+
+    // Отправляем AJAX
+    AJAX.lookup(
+      this.url + '?remove=' + seriesId + '&ajax=1',
+      function (cnt) {
+        // Аналогичное обновление счётчиков (как в add)
+        $$$('series-compare-cnt').innerHTML = cnt;
+        var spans = find('span.series-compare-cnt');
+        for (var i = 0, l = spans.length; i < l; i++) {
+          if (typeof spans[i] !== 'undefined') spans[i].innerHTML = cnt;
+        }
+
+        var desktopBadge = $$$('compare-badge-count-desktop');
+        var mobileBadge = $$$('compare-badge-count-mobile');
+        if (desktopBadge) {
+          desktopBadge.innerHTML = cnt;
+          cnt > 0 ? desktopBadge.classList.remove('hidden') : desktopBadge.classList.add('hidden');
+        }
+        if (mobileBadge) {
+          mobileBadge.innerHTML = cnt;
+          cnt > 0 ? mobileBadge.classList.remove('hidden') : mobileBadge.classList.add('hidden');
+        }
+      }
+    );
+  };
+}
 /*  search.js */
 /**
  * Имитация AJAX
