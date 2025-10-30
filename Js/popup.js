add446d9c6d780949d4291956c8de6eb730e7b3f
Dzmitry Kosmach <dzmitry.kosmach@gmail.com>
2025-10-30 19:53:59 +0300

    local245

--- Js/popup.js
+++ Js/popup.js
@@ -53,7 +53,7 @@ var oPopup = new function(){
 					autoSize		: false,
 					padding			: 20,
 					tpl				: {
-						closeBtn : '<a title="Close" class="fancybox-item fancybox-close" href="javascript:;"><i class="icon pngicons"></i></a>'
+						closeBtn : '<a title="Close" class="fancybox-item fancybox-close" href="javascript:;"><i class="icon text-red-600 hover:text-red-800 !text-4xl leading-none no-underline hover:no-underline">×</i></a>'
 					},
 					helpers			: {
 						title : {
@@ -77,7 +77,7 @@ var oPopup = new function(){
 					autoSize		: true,
 					padding			: 20,
 					tpl				: {
-						closeBtn : '<a title="Close" class="fancybox-item fancybox-close" href="javascript:;"><i class="icon pngicons"></i></a>'
+						closeBtn : '<a title="Close" class="fancybox-item fancybox-close" href="javascript:;"><i class="icon text-red-600 hover:text-red-800 !text-4xl leading-none no-underline hover:no-underline">×</i></a>'
 					},
 					helpers			: {
 						title : {
