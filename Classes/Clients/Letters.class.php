017a4dd7e8854b3938efcfb2f16ac1a8bdd7c426
Dzmitry Kosmach <dzmitry.kosmach@gmail.com>
2026-02-12 19:19:17 +0300

    local569

--- Classes/Clients/Letters.class.php
+++ Classes/Clients/Letters.class.php
@@ -47,6 +47,63 @@ class Clients_Letters extends ExtDbList {
 		$this->imageDel($ids);
 		return $result;
 	}
+
+
+    /**
+     * Сохраняем изображение письма и сразу создаём дополнительные размеры.
+     *
+     * При сохранении:
+     *  - оригинал: {id}.jpg
+     *  - дополнительные превью: {id}_220x312_0.jpg
+     *
+     * @param int    $id
+     * @param string $fileName
+     * @param string $fileRealName
+     * @return bool
+     */
+    function imageSave(int $id, string $fileName, string $fileRealName = ""): bool
+    {
+        // Сохраняем оригинал стандартным способом
+        $result = parent::imageSave($id, $fileName, $fileRealName);
+        if (!$result) {
+            return false;
+        }
+
+        // Путь к папке с картинками для писем
+        $path = $this->imagePath();
+        if (!$path) {
+            return true;
+        }
+
+        // Расширение сохранённого файла
+        $ext = $this->imageExt($id);
+        if (!$ext) {
+            return true;
+        }
+
+        // Полный путь к оригиналу
+        $originalFile = Config::path('images') . $path . $id . '.' . $ext;
+        if (!is_file($originalFile)) {
+            return true;
+        }
+
+        // Генерируем превью 220x312
+        $images = new Images();
+
+        $img = $images->fromFile($originalFile);
+        if ($img) {
+            // Обрезка под точный размер 220x312
+            $images->resize($img, 220, 312, IMG_RESIZE_CROP);
+
+            $dest = Config::path('images') . $path . $id . '_220x312_0.' . $ext;
+            $mime = Images::$extToMime[$ext] ?? Images::$extToMime['jpg'];
+
+            $images->ctrl->toFile($img, $dest, $mime);
+            $images->ctrl->destroy($img);
+        }
+
+        return true;
+    }
 }
 
 ?>
